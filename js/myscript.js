d24a7154da380c21042cdea4afe76441e3822911
Zivile <zivile@Zivile.local>
2016-11-09 17:41:25 +0100

    JS project for KEA curiculum

---
+++ js/myscript.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: true,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: true,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){labas}
+
+    function foo(){ console.log("Callback"); }
